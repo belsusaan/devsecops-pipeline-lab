645eaea9b5df5ab4439e6d405b0db0f2cbd19740
susana <susanabeltran679@gmail.com>
2026-09-04 19:20:54 -0600

    Agregar infraestructura Terraform

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+
+  type = string
+
+  default = "us-east-1"
+
+}
+
+
+
+variable "bucket_name" {
+
+  description = "bucket_name=devsecops-lab-beltran-2026"
+
+  type = string
+
+}
