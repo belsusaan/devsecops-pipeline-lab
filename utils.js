61a5e3ef2adeaca644d78b05bc8b78d02d6919d3
susana <susanabeltran679@gmail.com>
2026-09-12 09:36:46 -0600

    Agregar seguridad y calidad al pipeline (Lab 4)

---
+++ utils.js
@@ -0,0 +1,15 @@
+function calculateTotal(price, tax) {
+  return price + tax;
+}
+
+function getUserName(user) {
+  if (!user || !user.name) return "";
+
+  return user.name.toUpperCase();
+}
+
+function validateRange(value, min = 0, max = 120) {
+  return value >= min && value <= max;
+}
+
+module.exports = { calculateTotal, getUserName, validateRange };
