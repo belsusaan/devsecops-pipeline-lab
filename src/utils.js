b202461763de4071f1ad507c4d3a94b86fd12a12
susana <susanabeltran679@gmail.com>
2026-09-12 10:05:41 -0600

    Prueba de bloqueo: secreto de prueba

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,5 @@ function validateRange(value, min = 0, max = 120) {
 }
 
 module.exports = { calculateTotal, getUserName, validateRange };
+
+const apiKey = "sk_live_51Hxxxxxxxxxxxxxxxxxxxxxxxxx"; // secreto de prueba, se borra después
